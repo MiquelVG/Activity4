5d32a5fa89e5c7f4df3572b9c69166cf8a24eaa7
MiquelVidal <a2308927@institutmontilivi.cat>
2024-03-05 19:39:46 +0100

    InitialVersion

---
+++ pythonproject.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+print("You are a prisoner that has been held captive on a Spaceship for 6 months.\n Now it's your time to escape. A guard that you bribed will come at night time to open your cell.")
+print("The guard came and opened your cell, where will you go?")
+print("1-Go left")
+print("2-Go right")
+print("3-Go straight")
+option = int(input())
+while option < 1 and option > 3:
+    print("Not valid option. Try again: ")
+    option = int(input())
+if option == 1:
+    
+
+if option == 2:
+
+
+if option == 3:
